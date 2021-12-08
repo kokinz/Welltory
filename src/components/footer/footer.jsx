31266986600b99c1f82318b96de32585bdfad3f5
kokinz <nes-t@mail.ru>
2021-12-08 04:24:04 +0300

    style hover active done links

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 function Footer() {
   return (
@@ -9,56 +8,56 @@ function Footer() {
 
         <ul className="apps__list list">
           <li className="apps__item">
-            <Link to="/">
+            <a href="https://itunes.apple.com/app/welltory-personal-nyj-analitik/id1074367771?mt=8">
               <svg className="apps__image apps__image--apple" width="31" height="35" viewBox="0 0 31 35">
                 <use xlinkHref="#apple" />
               </svg>
-            </Link>
+            </a>
           </li>
 
           <li className="apps__item">
-            <Link to="/">
+            <a href="https://play.google.com/store/apps/details?id=com.welltory.client.android">
               <svg className="apps__image apps__image--google" width="29" height="33" viewBox="0 0 29 33">
                 <use xlinkHref="#play-market" />
               </svg>
-            </Link>
+            </a>
           </li>
         </ul>
       </div>
 
       <ul className="footer__links list">
         <li>
-          <Link className="footer__link link" to="/">Terms of use</Link>
+          <a className="footer__link link" href="https://welltory.com/terms/">Terms of use</a>
         </li>
 
         <li>
-          <Link className="footer__link link" to="/">Privacy policy</Link>
+          <a className="footer__link link" href="https://welltory.com/privacy/">Privacy policy</a>
         </li>
       </ul>
 
       <ul className="footer__social-links list">
         <li className="footer__social-link">
-          <Link to="/">
+          <a href="https://twitter.com/welltory">
             <img src="/img/twitter.png" alt="Twitter" />
-          </Link>
+          </a>
         </li>
 
         <li className="footer__social-link">
-          <Link to="/">
+          <a href="https://www.facebook.com/welltory">
             <img src="/img/facebook.png" alt="Facebook" />
-          </Link>
+          </a>
         </li>
 
         <li className="footer__social-link">
-          <Link to="/">
+          <a href="https://www.instagram.com/welltory/">
             <img src="/img/instagram.png" alt="Instagram" />
-          </Link>
+          </a>
         </li>
 
         <li className="footer__social-link">
-          <Link to="/">
+          <a href="https://www.linkedin.com/company/welltory">
             <img src="/img/linkedln.png" alt="Linkedln" />
-          </Link>
+          </a>
         </li>
       </ul>
     </footer>
